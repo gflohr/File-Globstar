cef8842fe325ff71db429640dc8e9acc92712fbd
Guido Flohr <guido.flohr@cantanea.com>
2017-10-13 14:50:52 +0300

    initial module structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# File-Globstar
+
+Perl Globstar (glob with double asterisk semantics)
+
+## Bugs
+
+Please report bugs at
+[https://github.com/gflohr/File-Globstar/issues](https://github.com/gflohr/File-Globstar/issues)
+
+## Copyright
+
+Copyright (C) 2016-2017, Guido Flohr, <guido.flohr@cantanea.com>,
+all rights reserved.
+
---
+++ lib/File/Globstar.pm
@@ -0,0 +1,12 @@
+# Copyright (C) 2016-2017 Guido Flohr <guido.flohr@cantanea.com>, 
+# all rights reserved.
+
+# This file is distributed under the same terms and conditions as
+# Perl itself.
+
+# This next lines is here to make Dist::Zilla happy.
+# ABSTRACT: Perl Globstar (double asterisk globbing) and utils
+
+package File::Globstar;
+
+1;
