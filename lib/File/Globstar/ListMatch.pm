facf151b5b61bdbfa7d28507f5d93acc23f1c924
Guido Flohr <guido.flohr@cantanea.com>
2017-10-17 00:40:35 +0300

    added File::Globstar::ListMatch

---
+++ lib/File/Globstar/ListMatch.pm
@@ -0,0 +1,17 @@
+# Copyright (C) 2016-2017 Guido Flohr <guido.flohr@cantanea.com>,
+# all rights reserved.
+
+# This file is distributed under the same terms and conditions as
+# Perl itself.
+
+# This next lines is here to make Dist::Zilla happy.
+# ABSTRACT: Perl Globstar (double asterisk globbing) and utils
+
+package File::Globstar::Match;
+
+use strict;
+
+use File::Globstar qw(translatestar);
+
+1;
+
